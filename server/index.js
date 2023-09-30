74dc6884516baf7494e405397ba669d7d7012092
Mohd Saquib Mansoori <msmsaq114@gmail.com>
2023-09-30 15:46:09 +0530

    Init

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const verifyProof = require('../utils/verifyProof');
+const niceList  = require('../utils/niceList')
+const MerkleTree = require('../utils/MerkleTree')
+
+
+const port = 1225;
+
+const app = express();
+app.use(express.json());
+
+const merkleTree =new MerkleTree(niceList);
+const MERKLE_ROOT =  merkleTree.getRoot();;
+
+app.post('/gift', (req, res) => {
+ 
+  const body = req.body;
+
+  // TODO: prove that a name is in the list 
+
+  const name  = body.requestedData.name;
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+  const isInTheList = verifyProof(proof,name,MERKLE_ROOT)
+  if(isInTheList) {
+    res.send("You got a toy robot!");
+  }
+  else {
+    res.send("You are not on the list :(");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
+
